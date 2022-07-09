ebb429394ebf4fd01110a500e8e1e02d061c0ce2
GaoNengwWw <gaonenggg@outlook.com>
2022-07-09 15:08:06 +0800

    fix(core): fix xmldom warning

--- packages/core/src/Box/box.ts
+++ packages/core/src/Box/box.ts
@@ -2,13 +2,19 @@ import { exit } from 'process';
 import { DOMParser } from 'xmldom';
 import { extract, xpath } from '../parser/parser';
 import { Box } from '../types/Box';
-
+const errorHandle = (level: string, msg: string)=>{
+    if (level !== 'warning'){
+        console.log(msg)
+    }
+}
 export function box(data: any, isFail: boolean): Box{
     let hasDoc = false;
     let doc;
     try{ 
         const html = data.toString().replace(/&/gim,'&amp;');
-        doc = new DOMParser().parseFromString(html);
+        doc = new DOMParser({
+            errorHandler: errorHandle
+        }).parseFromString(html);
         hasDoc = true;
     } catch (e) {
         doc = null;
