fdc3f9e79b8a408a0d05823aa33a5756056ce883
GaoNeng-wWw <gaonenggg@outlook.com>
2022-08-20 19:57:17 +0800

    🐳 chore(cli): 使用ts-compiler生成目标文件

--- packages/gachi-cli/src/action/runProject.js
+++ packages/gachi-cli/src/action/runProject.js
@@ -1,19 +1,36 @@
 const { exec } = require("child_process");
-const { readdirSync, realpathSync, readFileSync, fstat, writeFileSync } = require("fs");
+const { readdirSync, realpathSync, readFileSync, outputFileSync} = require("fs-extra");
+const glob = require('glob');
+const { join, resolve, basename } = require("path");
+const { createCompilerHost, createProgram } = require('typescript');
+
 
 function runProject(relativePath){
     let path = realpathSync(relativePath);
-    const runArgs = ``;
-    let command = ``
     let dir = readdirSync(path);
+    let tsConfig = JSON.parse(readFileSync(resolve(relativePath, 'tsconfig.json')).toString()).compilerOptions
     if (dir.includes('config.ts')){
-        exec('tsc', ()=>{
-            exec(`node ${relativePath}/dist/app.js`, (err, stdout, stderr)=>{
-                if (err)    {console.log(err)}
-                if (stdout) {console.log(stdout)}
-                if (stderr) {console.log(stderr)}
+        glob('**/*.ts', (err,matches)=>{
+            const files = matches.filter((fileName) => !fileName.includes('node_modules'));
+            const host = createCompilerHost(tsConfig);
+            const record = {};
+            host.writeFile = (fileName, text) => {
+                console.log(`[${new Date().getTime()}][gachi-cli][INFO]${fileName} Bundle Success`);
+                record[resolve(relativePath, fileName)] = text;
+            }
+            const program = createProgram(files, tsConfig, host);
+            program.emit();
+            const entries = Object.entries(record);
+            entries.forEach((v)=>{
+                const [path, content] = v;
+                outputFileSync(path, content);
+            })
+            exec(`node ${relativePath}/dist/app.js`, (error, stdout, stderr)=>{
+                if (error){console.log(error.message)}
+                console.log(stdout || stderr);
             })
         })
+        
     } else {
         console.warn('not find any Config File');
     }
